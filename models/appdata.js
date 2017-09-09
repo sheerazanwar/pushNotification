916d5f326e02c86da447fe55c505612f139c2318
Sheeraz Anwar <Sheeraz Anwar>
2017-09-09 18:52:36 +0500

    logic updated

--- models/appdata.js
+++ models/appdata.js
@@ -2,9 +2,9 @@ var mongoose = require('mongoose');
 
 var appDataSchema = new mongoose.Schema({
     title: { type: String, default: 'Name', lowercase: true },
-    type: { type: String, unique: true, dropDups: true },
+    type: { type: String,  dropDups: true },
     message: { type: String, default: 'Name', lowercase: true },
-    imageUrl: { type: String, unique: true, dropDups: true },
+    imageUrl: { type: String, dropDups: true },
     package: { type: String, unique: true, dropDups: true }
 });
 module.exports = mongoose.model('appData', appDataSchema);
