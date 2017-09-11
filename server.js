6e1499ca1d8bd0e8e28eb29e6d1716fd323eb001
Sheeraz Anwar <Sheeraz Anwar>
2017-09-11 13:02:07 +0500

    crash  resolved

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var routes = require('./routes/route.js');
 mongoose.Promise = require('bluebird');
 var app = express();
 
-mongoose.connect('process.env.mongoConnection');
+mongoose.connect('mongodb://sofittech:sofitpush@ds121622.mlab.com:21622/push');
 mongoose.connection.on('connected',function (){
   console.log('connected to database');
 });
